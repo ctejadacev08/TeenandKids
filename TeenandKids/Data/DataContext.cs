1794d1b87c9489a9c3029ddb6d864d9eb468fc60
Christian <tejada.utn08@hotmail.com>
2022-11-24 16:32:43 -0500

    Configuracion de EF, Creacion de la tabla Countries y Creacion de la BD

---
+++ TeenandKids/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using TeenandKids.Data.Entities;
+
+namespace TeenandKids.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Country> Countries { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+
+
+
+    }
+}
